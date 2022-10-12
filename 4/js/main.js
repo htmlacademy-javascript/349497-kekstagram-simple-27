1daa152b6b91a9be936f435955cc3ff60fe75164
Keks <keks@htmlacademy.ru>
2022-10-12 13:11:34 +0300

    :heavy_check_mark: Сборка #4

---
+++ 4/js/main.js
@@ -0,0 +1,4 @@
+import {getGenerateArrayObjects} from './util.js';
+import {GENERATE_OBJECTS} from './const.js';
+
+getGenerateArrayObjects(GENERATE_OBJECTS);
---
+++ 4/js/util.js
@@ -0,0 +1,34 @@
+import {Comments, Likes, DESCRIPTIONS} from './const.js';
+
+
+function isComformLength(checkedText, maxLength){
+  return checkedText.length <= maxLength;
+}
+
+function getRandomPositiveInteger (a, b) {
+  if (a < 0 || b < 0) {
+    return NaN;
+  }
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+}
+
+function getGenerateArrayObjects(quanty){
+  const arrObj = [];
+
+  for (let i = 1; i <= quanty; i++){
+    arrObj.push({
+      id: i,
+      url: `photos/${i}.jpg`,
+      description: DESCRIPTIONS[getRandomPositiveInteger(0, DESCRIPTIONS.length - 1)],
+      likes: getRandomPositiveInteger(Likes.MIN, Likes.MAX),
+      comments: getRandomPositiveInteger(Comments.MIN, Comments.MAX),
+    });
+  }
+
+  return arrObj;
+}
+
+export {isComformLength, getGenerateArrayObjects};
