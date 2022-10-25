2607807ef76ee6bbf7408a34074c145c8f7c60ce
menlor-ru <fedin.ee@yandex.ru>
2022-10-25 10:20:54 +0300

    9.8. Помощь друга

---
+++ js/form-effects.js
@@ -0,0 +1,49 @@
+import { imgUploadPreview, effectsList, effectSlider, effectSliderValue, defaultEffect } from './form-dom-element.js';
+import { Effects } from './const.js';
+
+const setClassEffectImg = (effect) =>{
+  imgUploadPreview.classList.remove(...imgUploadPreview.classList);
+  imgUploadPreview.classList.add(`effects__preview--${effect}`);
+};
+
+const setDefaultEffect = () => {
+  if (effectSlider.noUiSlider !== undefined){
+    effectSlider.noUiSlider.destroy();
+  }
+  setClassEffectImg('none');
+  imgUploadPreview.style.filter = '';
+  defaultEffect.checked = true;
+};
+
+const setSlider = ({filter, range, start, step, unit}) => {
+  if (effectSlider.noUiSlider !== undefined){ effectSlider.noUiSlider.destroy();}
+  noUiSlider.create(effectSlider, {
+    connect: true,
+    range: range,
+    start: start,
+    step: step,
+  });
+
+  effectSlider.noUiSlider.on('update', (values, handle) => {
+    effectSliderValue.value = values[handle];
+    imgUploadPreview.style.filter = '';
+
+    if (unit === undefined){
+      imgUploadPreview.style.filter = `${filter}(${effectSliderValue.value})`;
+      return;
+    }
+    imgUploadPreview.style.filter = `${filter}(${effectSliderValue.value}${unit})`;
+  });
+};
+
+const onChangeEffect = () => {
+  const effect = effectsList.querySelector('[name="effect"]:checked').value;
+  setClassEffectImg(effect);
+  if (effect === 'none'){
+    setDefaultEffect();
+    return;
+  }
+  setSlider(Effects[effect.toUpperCase()]);
+};
+
+export {onChangeEffect, setDefaultEffect};
